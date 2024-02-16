6504ba069136218eb204b29cb0d0b2b8c576f977
willlg <williandealmeida1@gmail.com>
2024-02-16 00:07:40 -0300

    search e account

--- src/app/components/components.module.ts
+++ src/app/components/components.module.ts
@@ -3,15 +3,16 @@ import { CommonModule } from '@angular/common';
 import { RestaurantComponent } from './restaurant/restaurant.component';
 import { IonicModule } from '@ionic/angular';
 import { LoadingRestaurantComponent } from './loading-restaurant/loading-restaurant.component';
+import { EmptyScreenComponent } from './empty-screen/empty-screen.component';
 
 
 
 @NgModule({
-  declarations: [RestaurantComponent, LoadingRestaurantComponent],
+  declarations: [RestaurantComponent, LoadingRestaurantComponent, EmptyScreenComponent],
   imports: [
     CommonModule,
-    IonicModule,
+    IonicModule
   ],
-  exports: [RestaurantComponent, LoadingRestaurantComponent]
+  exports: [RestaurantComponent, LoadingRestaurantComponent, EmptyScreenComponent]
 })
 export class ComponentsModule { }
