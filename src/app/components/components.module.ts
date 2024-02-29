9d67e296142ebc6ffa15134ab1208107b8c307f1
willlg <williandealmeida1@gmail.com>
2024-02-29 00:23:18 -0300

    cart arrumado e account component

--- src/app/components/components.module.ts
+++ src/app/components/components.module.ts
@@ -4,15 +4,15 @@ import { RestaurantComponent } from './restaurant/restaurant.component';
 import { IonicModule } from '@ionic/angular';
 import { LoadingRestaurantComponent } from './loading-restaurant/loading-restaurant.component';
 import { EmptyScreenComponent } from './empty-screen/empty-screen.component';
-
-
+import { AccountComponent } from './account/account.component';
+import { LoadingAccountComponent } from './loading-account/loading-account.component';
 
 @NgModule({
-  declarations: [RestaurantComponent, LoadingRestaurantComponent, EmptyScreenComponent],
+  declarations: [RestaurantComponent, LoadingRestaurantComponent, EmptyScreenComponent,AccountComponent,LoadingAccountComponent],
   imports: [
     CommonModule,
     IonicModule
   ],
-  exports: [RestaurantComponent, LoadingRestaurantComponent, EmptyScreenComponent]
+  exports: [RestaurantComponent, LoadingRestaurantComponent, EmptyScreenComponent, AccountComponent,LoadingAccountComponent]
 })
 export class ComponentsModule { }
--- src/app/pages/tabs/account/account.page.ts
+++ src/app/pages/tabs/account/account.page.ts
@@ -6,10 +6,52 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./account.page.scss'],
 })
 export class AccountPage implements OnInit {
+  orders=[];
+  isLoading: boolean = false;
 
   constructor() { }
 
   ngOnInit() {
+    this.LoadingAccount();
+    this.orders = [
+      {
+          name: 'Dom Henrique',
+          location: 'Guarapuava, Paraná',
+          price: 60.00,
+          status: 'Entregue',
+          items: ['Pizza x2', 'Burguer x2'],
+          date: 'Janeiro 29, 2024'
+        },
+        {
+          name: 'Dom Henrique ',
+          location: 'Guarapuava, Paraná',
+          price: 300.00,
+          status: 'Entregue',
+          items: ['Pizza x8'],
+          date: 'Janeiro 29, 2024'
+        },
+        {
+          name: 'Dom Henrique 2',
+          location: 'Guarapuava, Paraná',
+          price: 100.00,
+          status: 'Entregue',
+          items: ['Salada Especial x5'],
+          date: 'Janeiro 29, 2024'
+        },
+        {
+          name: 'Dom Henrique',
+          location: 'Guarapuava, Paraná',
+          price: 60.00,
+          status: 'Entregue',
+          items: ['Pizza x2', 'Burguer x2'],
+          date: 'Janeiro 29, 2024'
+        }
+    ];
   }
-
+  async LoadingAccount(){
+    this.isLoading = true;
+    setTimeout(() => {
+      this.isLoading = false;
+    }, 2000);
+  }
 }
